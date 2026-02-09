e96032383b6498687c7645396e780867f93e0613
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-09 09:09:53 -0700

    sticky header

--- components/ui/custom/header.tsx
+++ components/ui/custom/header.tsx
@@ -18,7 +18,7 @@ export function Header() {
         />
       )}
 
-      <div className="relative w-full z-20">
+      <div className="sticky top-0 w-full z-20">
         {/* Header bar */}
         <div className="flex items-center justify-between px-4 py-2 border-b bg-background">
           <NavigationMenuDemo />
