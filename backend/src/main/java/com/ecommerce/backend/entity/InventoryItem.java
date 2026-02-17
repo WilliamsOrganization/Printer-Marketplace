aa25575f265970ca3bbd2ca78f07b9ec4fb089c3
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-17 10:27:27 -0700

    added enums

--- backend/src/main/java/com/ecommerce/backend/entity/InventoryItem.java
+++ backend/src/main/java/com/ecommerce/backend/entity/InventoryItem.java
@@ -1,26 +1,27 @@
 package com.ecommerce.backend.entity;
 
-import java.sql.Date;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.sql.Date;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 /**
- * InventoryItem This is a marketplace Item in which to market 
+ * InventoryItem This is a marketplace Item in which to market
  */
 @Data
 @Entity
 @Table(name = "InventoryItem")
 public class InventoryItem {
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@CreationTimestamp
 	private Date createdAt;
@@ -28,10 +29,22 @@ public class InventoryItem {
 	private Date updatedAt;
 
 	private String itemTitle;
-	private String itemDescription;
+	private String item;
 	private Long itemCost;
 	private String imageUrl;
 	private String stripeId;
 	private Boolean sale;
 
+	@Enumerated(EnumType.STRING)
+	private Category category;
+
+	@Enumerated(EnumType.STRING)
+	private Badge badge;
+
+	public enum Category {
+		ELECTRONICS, PRINTS, CUSTOM
+	}
+	public enum Badge {
+		BESTSELLER, NEW, SALE
+	}
 }
