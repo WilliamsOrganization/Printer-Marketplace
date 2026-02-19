1ca31a30a304fb17e30fd15a434364a305db6928
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-19 05:34:31 -0700

    small form fix

--- frontend/src/app/admin/item/page.tsx
+++ frontend/src/app/admin/item/page.tsx
@@ -23,7 +23,7 @@ function page() {
 	}
 	return (
 		<div className="bg-muted flex min-h-[70vh] flex-col items-center justify-between gap-6 p-6 md:p-10">
-			<div className="flex w-full max-w-1/2 flex-col gap-6">
+			<div className="flex  w-full max-w-full flex-col items-center justify-center gap-6">
 				<a href="#" className="flex items-center gap-2 self-center font-medium">
 					<div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
 						<GalleryVerticalEnd className="size-4" />
