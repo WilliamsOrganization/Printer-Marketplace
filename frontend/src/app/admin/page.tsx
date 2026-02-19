5ebc29306a7ca3323714dc8b1611f943ce2e3c14
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-18 23:54:27 -0700

    nginx now working

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -14,8 +14,18 @@ export default function LoginPage() {
 		)
 	}
   return (
-    <div className="bg-muted flex min-h-[70vh] flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
+    <div className="bg-muted flex min-h-[70vh] flex-col items-center justify-between gap-6 p-6 md:p-10">
+      <div className="flex w-1/2 max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Acme Inc.
+        </a>
+        <LoginForm />
+      </div>
+
+      <div className="flex w-1/2 max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <GalleryVerticalEnd className="size-4" />
