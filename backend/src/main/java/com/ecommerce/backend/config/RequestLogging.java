5ae402a02a1681fe6bebc85519cb0f1bc7e31e9d
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-18 21:24:11 -0700

    added request logging.

---
+++ backend/src/main/java/com/ecommerce/backend/config/RequestLogging.java
@@ -0,0 +1,23 @@
+package com.ecommerce.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+/**
+ * RequestLogging
+ */
+@Configuration
+public class RequestLogging {
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter(){
+		CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(true);
+		filter.setMaxPayloadLength(1000);
+		filter.setIncludeHeaders(false);
+		filter.setBeforeMessagePrefix("INCOMING REQUEST: ");
+		filter.setAfterMessagePrefix("REQUEST COMPLETE: ");
+		return filter;
+	}
+}
