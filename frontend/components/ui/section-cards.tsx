415e257b0e4456e687e418ee948a13db06c51ee6
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-19 02:14:09 -0700

    moved testing json to different location.

--- frontend/components/ui/section-cards.tsx
+++ frontend/components/ui/section-cards.tsx
@@ -13,9 +13,10 @@ import {
 export function SectionCards() {
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+	{/* this is the important card I want to actually mess with */}
       <Card className="@container/card">
         <CardHeader>
-          <CardDescription>Total Revenue</CardDescription>
+          <CardDescription>Create New Item</CardDescription>
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
             $1,250.00
           </CardTitle>
@@ -35,6 +36,7 @@ export function SectionCards() {
           </div>
         </CardFooter>
       </Card>
+
       <Card className="@container/card">
         <CardHeader>
           <CardDescription>New Customers</CardDescription>
