5727afac3108b40dab2ded5da816d425ff2a9581
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-19 17:25:09 -0700

    lots of hardcoded urls for local development to work

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+	async rewrites() {
+		return [
+			{
+				source:"/server/:path*",
+				destination: 'http://backend:8080/server/:path*'
+			}
+
+		]
+	}
 };
 
 export default nextConfig;
