e4c98edfa7c4b9f0046906a86a572a963e6ff520
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-21 02:25:59 -0700

    fixed the middleware nonsense left working component level protection.

--- frontend/src/app/admin/dashboard/item/page.tsx
+++ frontend/src/app/admin/dashboard/item/page.tsx
@@ -6,21 +6,21 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 function page() {
-	const { data: session, status } = useSession();
-	if (status === "unauthenticated") {
-		return (
-			<div className="flex flex-col items-center justify-center min-h-[70vh] px-6">
-				<h1 className="text-6xl font-bold text-muted-foreground">Oops!</h1>
-				<h2 className="mt-4 text-xl font-semibold">Access Denied</h2>
-				<p className="mt-2 text-muted-foreground text-center max-w-md">
-					You need to be logged in to visit this page
-				</p>
-				<Button asChild className="mt-6">
-					<Link href="/admin/">Back to Admin Login</Link>
-				</Button>
-			</div>
-		);
-	}
+	// const { data: session, status } = useSession();
+	// if (status === "unauthenticated") {
+	// 	return (
+	// 		<div className="flex flex-col items-center justify-center min-h-[70vh] px-6">
+	// 			<h1 className="text-6xl font-bold text-muted-foreground">Oops!</h1>
+	// 			<h2 className="mt-4 text-xl font-semibold">Access Denied</h2>
+	// 			<p className="mt-2 text-muted-foreground text-center max-w-md">
+	// 				You need to be logged in to visit this page
+	// 			</p>
+	// 			<Button asChild className="mt-6">
+	// 				<Link href="/admin/">Back to Admin Login</Link>
+	// 			</Button>
+	// 		</div>
+	// 	);
+	// }
 	return (
 		<div className="bg-muted flex min-h-[70vh] flex-col items-center justify-between gap-6 p-6 md:p-10">
 			<div className="flex  w-full max-w-full flex-col items-center justify-center gap-6">
