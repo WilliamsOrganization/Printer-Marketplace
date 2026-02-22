362649bce8991287795cc6b97c6cacdfc99d5a4b
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-22 03:07:54 -0700

    cleaning up logs

--- frontend/components/ui/custom/add-to-cart-button.tsx
+++ frontend/components/ui/custom/add-to-cart-button.tsx
@@ -21,11 +21,6 @@ export function AddToCartButton({
 			})
 			.then((res) => {
 				toast.success("Item successfuly added to cart");
-				console.log("res.data: " + JSON.stringify(res.data));
-
-				console.log("previous cart:", cart);
-				console.log("res.data:", res.data);
-
 				setCart((previous) => {
 					const updatedItems = previous!.items.concat(res.data);
 					const updatedCart = { ...previous!, items: updatedItems };
