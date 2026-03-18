04fdf3a7dd5b311ff43fa7c6447e5edbaab50aad
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-03-18 03:54:11 -0600

    terraform apply now working for the debian instance on the plex server

--- terraform/starter.tf
+++ terraform/starter.tf
@@ -9,21 +9,58 @@ terraform {
 }
 
 variable "virtual_environment_endpoint" { type = string }
-variable "virtual_environment_api_token" { type = string }
-variable "virtual_environment_username" { type = string }
-variable "virtual_environment_password" { type = string }
-variable "virtual_environment_auth_ticket" { type = string }
-variable "virtual_environment_csrf_prevention_token" { type = string }
+variable "virtual_environment_secret" { type = string }
+variable "ssh_public_key_file" { type = string }
 
 provider "proxmox" {
-  endpoint = var.virtual_environment_endpoint
-  api_token = var.virtual_environment_api_token
+  endpoint  = var.virtual_environment_endpoint 
+  api_token = var.virtual_environment_secret
   # username =  var.virtual_environment_username
   # password = var.virtual_environment_password
   insecure = true
 
   ssh {
-    agent = true
-    username = "root"  # required when using api_token
+    agent    = true
+    username = "root" # required when using api_token
   }
+}
+
+resource "proxmox_virtual_environment_container" "debian" {
+  node_name   = "plex" # your proxmox node name
+  unprivileged = true
+
+  initialization {
+    hostname = "debian-test"
+    user_account {
+      keys = [file(var.ssh_public_key_file)]
+    }
+
+    ip_config {
+      ipv4 {
+        address = "192.168.1.54/24"
+        gateway = "192.168.1.1"
+      }
+    }
+  }
+
+  network_interface {
+    name = "eth0"
+  }
+
+  operating_system {
+    template_file_id = "local:vztmpl/debian-12-standard_12.12-1_amd64.tar.zst"
+    type             = "debian"
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 8
+  }
+
+  cpu { cores = 1 }
+  memory { dedicated = 512 }
+  features {
+    nesting = true
+  }
+
 }
