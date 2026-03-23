fe6cbf88b283872707294086fcfcb77092752e8d
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-03-22 18:25:03 -0600

    added email todo

--- backend/src/main/java/com/ecommerce/backend/service/ResendService.java
+++ backend/src/main/java/com/ecommerce/backend/service/ResendService.java
@@ -22,6 +22,7 @@ public class ResendService {
 	@Value("${resend.email_from}")
 	private String from;
 
+	// TODO: fix me for later to have 3 emailing endpoints for users table and order confirmation tables
 	public void testEmailEndpoint(String toEmail, String body) {
 		CreateEmailOptions params = CreateEmailOptions.builder()
 				.from("noreply@printmarket.ca")
