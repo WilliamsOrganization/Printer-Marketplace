2c811e367663dc9994d2a88eb47534a1005b4c0a
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-09-25 00:04:53 -0600

    BUG: prerendering bug

--- frontend/src/app/admin/(dashboard)/analytics/page.tsx
+++ frontend/src/app/admin/(dashboard)/analytics/page.tsx
@@ -1,6 +1,8 @@
 import { AnalyticsView } from "@/components/ui/custom/analytics-view"
 import { DashboardOverview } from "@/components/ui/custom/dashboard-overview"
 
+export const dynamic = "force-dynamic"
+
 export default function AnalyticsPage() {
 	return (
 		<>
