9b58e3ee1c2d07c044f93d73fe58f4d6b5e88818
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-09-25 22:11:54 -0600

    fix

--- frontend/src/app/admin/(dashboard)/analytics/page.tsx
+++ frontend/src/app/admin/(dashboard)/analytics/page.tsx
@@ -1,8 +1,6 @@
 import { AnalyticsView } from "@/components/ui/custom/analytics-view"
 import { DashboardOverview } from "@/components/ui/custom/dashboard-overview"
 
-export const dynamic = "force-dynamic"
-
 export default function AnalyticsPage() {
 	return (
 		<>
